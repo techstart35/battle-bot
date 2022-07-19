ab43325ec0a6f1b84403883bc17d5ffc992c53e6
totsuka <totsuka@totsukanoMacBook-Air.local>
2022-07-19 12:44:06 +0900

    自爆確率を変更

--- discord/message/battle/battle.go
+++ discord/message/battle/battle.go
@@ -288,7 +288,6 @@ func createBattleMessage(entryMessage *discordgo.Message, stage []*discordgo.Use
 		soloNoBattle = iota + 1 // 3
 	)
 
-	seedCount := 0
 	for {
 		// キャンセル指示を確認
 		if !shared.IsProcessing[entryMessage.ChannelID] {
@@ -300,11 +299,11 @@ func createBattleMessage(entryMessage *discordgo.Message, stage []*discordgo.Use
 		// 2つ取得可能な場合のみ、ランダムで取得する
 		if nextUsersIndex+1 != len(stage) {
 			tmpWaitList := []int{
-				// soloBattle: 20%
+				// soloBattle: 30%
 				soloBattle,
 				soloBattle,
-				// soloNoBattle: 40%
-				soloNoBattle,
+				soloBattle,
+				// soloNoBattle: 30%
 				soloNoBattle,
 				soloNoBattle,
 				soloNoBattle,
@@ -316,8 +315,7 @@ func createBattleMessage(entryMessage *discordgo.Message, stage []*discordgo.Use
 			}
 
 			// ランダムにするため、スライスをシャッフル
-			wl := shared.ShuffleInt(tmpWaitList, seedCount)
-			seedCount++
+			wl := shared.ShuffleInt(tmpWaitList, nextUsersIndex)
 
 			num = wl[shared.RandInt(1, 11)-1]
 		}
